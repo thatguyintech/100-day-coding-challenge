6c7cac95cd65b312957e58d4a264263d076cec8f
Albert Hu <alberthu@berkeley.edu>
2017-05-28 02:22:31 -0700

    Day 34: use regex matching instead of subset check and combine inequality checks

--- day34/validateIP.py
+++ day34/validateIP.py
@@ -1,58 +1,56 @@
+import re
+
 def validateIP(ip):
     ipv4 = ip.split(".")
     ipv6 = ip.split(":")
-    if len(ipv4) == 4 and validateIPv4(ipv4):
+    if len(ipv4) == 4 and isValidIPv4(ip):
         return "IPv4"
-    elif len(ipv6) == 8 and validateIPv6(ipv6):
+    elif len(ipv6) == 8 and isValidIPv6(ip):
         return "IPv6"
     else:
         return "Neither"
 
-def validateIPv4(segments):
-    digits = set("1234567890")
-    for s in segments:
-        # segment too long or too short
-        if len(s) > 3 or len(s) == 0:
+def isValidIPv4(ip):
+    for s in ip.split("."):
+        numDigits = len(s)
+
+        if not 1 <= numDigits <= 3:
             return False
 
-        # segment has leading 0
-        if len(s) > 1 and s[0] == "0":
+        if s.startswith("0"):
             return False
 
-        # segment contains non-numeric digits
-        if not set(s) < digits:
+        if not re.match("[1234567890]{{{}}}".format(numDigits), s):
             return False
 
-        # segment is not an int between 0 and 255 inclusive
-        if int(s) > 255 or int(s) < 0:
+        if not 0 <= int(s) <= 255:
             return False
     return True
 
-def validateIPv6(segments):
-    hexDigits = set("1234567890abcdefABCDEF")
-    for s in segments:
-        # segment too long or too short
-        if len(s) > 4 or len(s) == 0:
+def isValidIPv6(ip):
+    for s in ip.split(":"):
+        numDigits = len(s)
+
+        if not 1 <= numDigits <= 4:
             return False
 
-        # segment contains non-hexadecimal digits
-        if not set(s) < hexDigits:
+        if not re.match("[1234567890abcdefABCDEF]{{{}}}".format(numDigits), s):
             return False
     return True
 
-def testValidateIPv4():
-    assert validateIPv4(["172", "16", "254", "1"])
-    assert not validateIPv4(["172", "16", "254", "01"])
-    assert not validateIPv4(["172", "16", "256", "1"])
-    assert not validateIPv4(["1e1", "4", "5", "6"])
-    assert not validateIPv4(["1e1", "", "5", "6"])
+def testIsValidIPv4():
+    assert isValidIPv4("172.16.254.1")
+    assert not isValidIPv4("172.16.254.01")
+    assert not isValidIPv4("172.16.256.1")
+    assert not isValidIPv4("1e1.4.5.6")
+    assert not isValidIPv4("1e1..5.6")
 
-def testValidateIPv6():
-    assert validateIPv6(["2001", "0db8", "85a3", "0000", "0000", "8a2e", "0370", "7334"])
-    assert validateIPv6(["2001", "db8", "85a3", "0", "0", "8A2E", "0370", "7334"])
-    assert not validateIPv6(["2001", "0db8", "85a3", "", "", "8A2E", "0370", "7334"])
-    assert not validateIPv6(["02001", "0db8", "85a3", "0000", "0000", "8a2e", "0370", "7334"])
-    assert not validateIPv6(["GGGG", "0db8", "85a3", "0000", "0000", "8a2e", "0370", "7334"])
+def testIsValidIPv6():
+    assert isValidIPv6("2001:0db8:85a3:0000:0000:8a2e:0370:7334")
+    assert isValidIPv6("2001:db8:85a3:0:0:8A2E:0370:7334")
+    assert not isValidIPv6("2001:0db8:85a3:::8A2E:0370:7334")
+    assert not isValidIPv6("02001:0db8:85a3:0000:0000:8a2e:0370:7334")
+    assert not isValidIPv6("GGGG:0db8:85a3:0000:0000:8a2e:0370:7334")
 
 def testValidateIP():
     assert validateIP("172.16.254.1") == "IPv4"
@@ -66,8 +64,8 @@ def testValidateIP():
     assert validateIP("02001:db8:85a3:0:0:8A2E:0370:7334") == "Neither"
 
 def main():
-    testValidateIPv4()
-    testValidateIPv6()
+    testIsValidIPv4()
+    testIsValidIPv6()
     testValidateIP()
 
 if __name__ == "__main__":
